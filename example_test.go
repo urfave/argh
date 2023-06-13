ccac0ff151b371f7cb5e66d44f255276e5817c3b
Dan Buch <dan@meatballhat.com>
2023-06-13 09:57:31 -0400

    Move example executable into example test

---
+++ example_test.go
@@ -0,0 +1,39 @@
+package argh_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"git.meatballhat.com/x/argh"
+)
+
+func ExampleArgh_simple() {
+	pCfg := argh.NewParserConfig()
+	pCfg.Prog = &argh.CommandConfig{
+		NValue:     argh.OneOrMoreValue,
+		ValueNames: []string{"val"},
+		Flags: &argh.Flags{
+			Automatic: true,
+		},
+	}
+
+	// simulate command line args
+	os.Args = []string{"hello", "there"}
+
+	pt, err := argh.ParseArgs(os.Args, pCfg)
+	if err != nil {
+		argh.PrintParserError(os.Stderr, err)
+		os.Exit(86)
+		return
+	}
+
+	if pt == nil {
+		log.Fatal("no parse tree?")
+	}
+
+	fmt.Printf("parsed!\n")
+
+	// Output:
+	// parsed!
+}
