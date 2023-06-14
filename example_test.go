b9823d73cb5f9590e691a285b620cd145f8792e5
Dan Buch <dan@meatballhat.com>
2023-06-14 08:48:14 -0400

    Make simple example slightly less simple

--- example_test.go
+++ example_test.go
@@ -14,12 +14,26 @@ func ExampleParserConfig_simple() {
 		NValue:     argh.OneOrMoreValue,
 		ValueNames: []string{"val"},
 		Flags: &argh.Flags{
-			Automatic: true,
+			Map: map[string]argh.FlagConfig{
+				"a": {
+					NValue: 2,
+					On: func(cf argh.CommandFlag) {
+						fmt.Printf("prog -a Name: %[1]q\n", cf.Name)
+						fmt.Printf("prog -a Values: %[1]q\n", cf.Values)
+						fmt.Printf("prog -a len(Nodes): %[1]v\n", len(cf.Nodes))
+					},
+				},
+			},
+		},
+		On: func(cf argh.CommandFlag) {
+			fmt.Printf("prog Name: %[1]q\n", cf.Name)
+			fmt.Printf("prog Values: %[1]q\n", cf.Values)
+			fmt.Printf("prog len(Nodes): %[1]v\n", len(cf.Nodes))
 		},
 	}
 
 	// simulate command line args
-	os.Args = []string{"hello", "there"}
+	os.Args = []string{"hello", "-a=from", "the", "ether"}
 
 	pt, err := argh.ParseArgs(os.Args, pCfg)
 	if err != nil {
@@ -32,8 +46,11 @@ func ExampleParserConfig_simple() {
 		log.Fatal("no parse tree?")
 	}
 
-	fmt.Printf("parsed!\n")
-
 	// Output:
-	// parsed!
+	// prog -a Name: "a"
+	// prog -a Values: map["0":"from" "1":"the"]
+	// prog -a len(Nodes): 4
+	// prog Name: "hello"
+	// prog Values: map["val":"ether"]
+	// prog len(Nodes): 4
 }
