34b83847b2c77fa6d037894372858a79102ff926
Dan Buch <dan@meatballhat.com>
2023-07-08 05:54:13 -0400

    Re-add nvalue stringer-generated bits

--- parser_config.go
+++ parser_config.go
@@ -1,42 +1,5 @@
 package argh
 
-const (
-	OneOrMoreValue  NValue = -2
-	ZeroOrMoreValue NValue = -1
-	ZeroValue       NValue = 0
-)
-
-var (
-	zeroValuePtr = func() *NValue {
-		v := ZeroValue
-		return &v
-	}()
-)
-
-type NValue int
-
-func (nv NValue) Required() bool {
-	if nv == OneOrMoreValue {
-		return true
-	}
-
-	return int(nv) >= 1
-}
-
-func (nv NValue) Contains(i int) bool {
-	tracef("NValue.Contains(%v)", i)
-
-	if i < int(ZeroValue) {
-		return false
-	}
-
-	if nv == OneOrMoreValue || nv == ZeroOrMoreValue {
-		return true
-	}
-
-	return int(nv) > i
-}
-
 type ParserConfig struct {
 	Prog *CommandConfig
 
